499036e7c4f7d12477dfe0bc5a3a59ca425ab61c
rdotlee <rdotlee@gmail.com>
2015-05-11 09:50:11 -0500

    create delete read

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,25 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @p = Photo.find(params["id"])
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.source=params["the_source"]
+    p.caption=params["the_caption"]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+  def destroy
+    p = Photo.find(params["id"])
+    p.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
 
 end
